ee4492d2ab7226f502839f6a2738e1cec5bcedfc
Reto Buerki <reet@codelabs.ch>
2012-09-26 15:15:34 +0200

    Connect to Esa Event Service (EES) on startup

--- src/xfrm_proxy.adb
+++ src/xfrm_proxy.adb
@@ -1,15 +1,20 @@
 with Ada.Text_IO;
 with Ada.Exceptions;
 
-with Interfaces.C;
+with Interfaces.C.Strings;
 
 with Anet.Sockets.Netlink;
 with Anet.Sockets.Dgram_Receiver;
+
+with Tkmrpc.Results;
+with Tkmrpc.Clients.Ees;
 
 with Callbacks;
 
 procedure Xfrm_Proxy
 is
+
+   use type Tkmrpc.Results.Result_Type;
 
    package Netlink_Receiver is new Anet.Sockets.Dgram_Receiver
      (Socket_Type  => Anet.Sockets.Netlink.Raw_Socket_Type,
@@ -19,8 +24,13 @@ is
    function C_Getpid return Interfaces.C.int;
    pragma Import (C, C_Getpid, "getpid");
 
+   EES_Socket : constant String := "/tmp/tkm.rpc.ees";
+   EES_Status : Tkmrpc.Results.Result_Type;
+
    Sock : aliased Anet.Sockets.Netlink.Raw_Socket_Type;
    Rcvr : Netlink_Receiver.Receiver_Type (S => Sock'Access);
+
+   RPC_Error : exception;
 begin
    Sock.Init (Protocol => Anet.Sockets.Netlink.Proto_Netlink_Xfrm);
    Sock.Bind (Address => Integer (C_Getpid),
@@ -28,6 +38,13 @@ begin
                           2 => Anet.Sockets.Netlink.Group_Xfrm_Expire));
 
    Rcvr.Listen (Callback => Callbacks.Handle_Message'Access);
+
+   Tkmrpc.Clients.Ees.Init (Result  => EES_Status,
+                            Address => Interfaces.C.Strings.New_String
+                              (EES_Socket));
+   if EES_Status /= Tkmrpc.Results.Ok then
+      raise RPC_Error with "Could not bind to UNIX path " & EES_Socket;
+   end if;
 
 exception
    when E : others =>
