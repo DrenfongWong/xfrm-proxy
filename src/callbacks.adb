4f58a23eee767f34251b967175a25afe2620c08f
Reto Buerki <reet@codelabs.ch>
2012-09-26 15:16:26 +0200

    Call Esa_[Acquire|Expire] on associated XFRM events

--- src/callbacks.adb
+++ src/callbacks.adb
@@ -1,9 +1,14 @@
 with Ada.Text_IO;
+with Ada.Exceptions;
 
 with System;
 
 with asm_generic_int_ll64_h;
 with xfrm_h;
+
+with Tkmrpc.Types;
+with Tkmrpc.Results;
+with Tkmrpc.Clients.Ees;
 
 with Xfrm;
 
@@ -77,6 +82,9 @@ is
             case Attr_Kind is
                when xfrm_h.XFRMA_TMPL =>
                   declare
+                     use type Tkmrpc.Results.Result_Type;
+
+                     Status    : Tkmrpc.Results.Result_Type;
                      Tmpl_Addr : constant System.Address
                        := Xfrm.Rta_Data (Rta => Rta'Access);
                      Tmpl      : xfrm_h.xfrm_user_tmpl;
@@ -84,6 +92,17 @@ is
                      pragma Import (Ada, Tmpl);
                   begin
                      Ada.Text_IO.Put_Line ("Reqid :" & Tmpl.reqid'Img);
+                     Tkmrpc.Clients.Ees.Esa_Acquire
+                       (Result => Status,
+                        Sp_Id  => Tkmrpc.Types.Sp_Id_Type (Tmpl.reqid));
+                     if Status /= Tkmrpc.Results.Ok then
+                        Ada.Text_IO.Put_Line ("Esa_Acquire failed");
+                     end if;
+
+                  exception
+                     when E : others =>
+                        Ada.Text_IO.Put_Line
+                          (Ada.Exceptions.Exception_Information (E));
                   end;
                when others =>
                   Ada.Text_IO.Put_Line ("Skipping RTA " & Attr_Kind'Img);
@@ -101,17 +120,34 @@ is
    procedure Process_Expire (Message : access Xfrm.Nlmsghdr_Type)
    is
       use type asm_generic_int_ll64_h.uu_u8;
+      use type Tkmrpc.Results.Result_Type;
 
+      Status : Tkmrpc.Results.Result_Type;
       Addr   : constant System.Address
         := Xfrm.Nlmsg_Data (Msg => Message);
       Expire : xfrm_h.xfrm_user_expire;
       for Expire'Address use Addr;
       pragma Import (Ada, Expire);
    begin
+
       Ada.Text_IO.Put_Line ("Proto :" & Expire.state.id.proto'Img);
       Ada.Text_IO.Put_Line ("SPI   :" & Expire.state.id.spi'Img);
       Ada.Text_IO.Put_Line ("Reqid :" & Expire.state.reqid'Img);
       Ada.Text_IO.Put_Line ("Hard  : " & Boolean'Image (Expire.hard /= 0));
+      Tkmrpc.Clients.Ees.Esa_Expire
+        (Result   => Status,
+         Sp_Id    => Tkmrpc.Types.Sp_Id_Type (Expire.state.reqid),
+         Spi_Rem  => Tkmrpc.Types.Esp_Spi_Type (Expire.state.id.spi),
+         Protocol => Tkmrpc.Types.Protocol_Type (Expire.state.id.proto),
+         Hard     => Tkmrpc.Types.Expiry_Flag_Type (Expire.hard));
+      if Status /= Tkmrpc.Results.Ok then
+         Ada.Text_IO.Put_Line ("Esa_Expire failed");
+      end if;
+
+   exception
+      when E : others =>
+         Ada.Text_IO.Put_Line
+           (Ada.Exceptions.Exception_Information (E));
    end Process_Expire;
 
 end Callbacks;
